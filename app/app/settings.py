9e281b10dac697af5cc0e363514fecaae94d386b
Jihyun <034179@naver.com>
2022-01-31 20:00:00 +0900

    31. Added custom user model.

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User' # 사용자 지정 사용자 모델로 지정
+
